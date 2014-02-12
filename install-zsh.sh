d6901c60880e3c0ae60c68e5037641ff766ea92e
Chenguang Zhang <chenguang.zhang@hulu.com>
2014-02-12 20:19:26 +0800

    Add instructions about how to set DEFAULT SHELL to zsh

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,2 +1,16 @@
+# Please install zsh first
+
+# For Mac OSX
+# brew install zsh
+
+# For Ubuntu
+# sudo apt-get install zsh
+
+# If default $SHELL is not changed to zsh
+# add the following line to .bash_profile
+
+#   SHELL=$ZSH_PATH exec $ZSH_PATH --login
+# ($ZSH_PATH can be /usr/local/bin/zsh OR /usr/bin/zsh)
+
 curl -L github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
